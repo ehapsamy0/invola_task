11bdfebb4b17819194bf851797c0726db66f7237
ehapsamy0 <ehapsamy434@gmail.com>
2022-10-22 12:36:09 +0200

    add srialize to status and make mare serialize to shipment details

--- shipment/serializers.py
+++ shipment/serializers.py
@@ -1,11 +1,35 @@
 
+from os import stat
 from rest_framework import serializers
-from shipment.models import Shipment
+from shipment.models import Shipment,ShipmentStatus
+from courier.models import Courier
+from courier.serializers import CourierSerializer
 
+
+
+class StatusSipmentSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = ShipmentStatus
+        fields = "__all__"
 
 
 class ShipmentSerializer(serializers.ModelSerializer):
+    courier = serializers.SerializerMethodField(read_only=True)
+    status = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Shipment
         fields = "__all__"
+
+
+    def get_courier(self,obj):
+        courier = obj.courier
+        serializer = CourierSerializer(courier,many=False).data
+        return serializer
+
+    def get_status(self,obj):
+        status = obj.status
+        serializer = StatusSipmentSerializer(status,many=False).data
+        return serializer
